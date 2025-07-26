cef4467ea11ea0bc9b8cdab8a6f71a2daceb6ab5
MirnaMilad <mirnamilad0101@gmail.com>
2025-07-26 14:52:51 +0300

    chore: add husky pre-commit hook with lint and prettier configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,96 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+// eslint.config.js
+import js from "@eslint/js";
+import globals from "globals";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+import importPlugin from "eslint-plugin-import";
+import { globalIgnores } from "eslint/config";
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+export default tseslint.config(
+  // Ignore build folders and config files
+  globalIgnores([
+    "vite.config.ts",
+    "commitlint.config.js",
+    "node_modules",
+    "dist",
+    "build",
+    "coverage",
+  ]),
+  // Shared plugins (exclude react-hooks, as it's already in recommended configs)
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    plugins: {
+      react,
+      import: importPlugin,
     },
   },
-])
+  // JS files
+  {
+    files: ["**/*.{js,jsx}"],
+    extends: [js.configs.recommended],
+    languageOptions: {
+      ecmaVersion: 2023,
+      sourceType: "module",
+      globals: globals.browser,
+    },
+    rules: {
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+  // TS / TSX files
+  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+      reactHooks.configs["recommended-latest"], // includes react-hooks plugin
+      eslintConfigPrettier,
+    ],
+    languageOptions: {
+      ecmaVersion: 2023,
+      sourceType: "module",
+      globals: globals.browser,
+      parserOptions: {
+        project: ["./tsconfig.json"],
+      },
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      "@typescript-eslint/explicit-function-return-type": [
+        "error",
+        { allowExpressions: false },
+      ],
+      "@typescript-eslint/explicit-module-boundary-types": "error",
+      "@typescript-eslint/no-explicit-any": [
+        "error",
+        { ignoreRestArgs: false },
+      ],
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        { fixStyle: "inline-type-imports" },
+      ],
+      "@typescript-eslint/no-floating-promises": "error",
+      "@typescript-eslint/no-misused-promises": [
+        "error",
+        { checksVoidReturn: { attributes: false } },
+      ],
+      "@typescript-eslint/prefer-readonly": "error",
+
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+    },
+  },
+);
