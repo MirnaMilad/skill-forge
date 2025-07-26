f8bfb81a6aa1cc5339c4b71f267bae30349bb970
MirnaMilad <mirnamilad0101@gmail.com>
2025-07-26 21:31:19 +0300

    feat(header): create Header component with logo and base styles

---
+++ src/layouts/MainLayouts/content-layout/Content-layout.tsx
@@ -0,0 +1,12 @@
+import React, { type FC } from "react";
+import Header from "../../components/Header";
+
+const ContentLayout: FC = () => {
+  return (
+    <div className="container-fluid h-100">
+      <Header />
+    </div>
+  );
+};
+
+export default ContentLayout;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./assets/styles/theme.css";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
